359944630b615723327a84b34e81f0a0ced0a855
Ted <hu1314v5@outlook.com>
2016-09-14 18:25:10 +0800

    mediaUtils工具增加 获取专辑封面 增加 主页底部音乐控制器

--- AndroidStudioProjects/MyMusic/app/src/main/java/com/ted/mymusic/com/ted/mymusic/bean/Music.java
+++ AndroidStudioProjects/MyMusic/app/src/main/java/com/ted/mymusic/com/ted/mymusic/bean/Music.java
@@ -1,5 +1,7 @@
 package com.ted.mymusic.com.ted.mymusic.bean;
 
+import android.graphics.Bitmap;
+
 /**
  * 音乐bean
  * Created by TED on 2016/9/14.
@@ -9,11 +11,13 @@ public class Music {
     public String artist; // 歌手
     public String path; //路径
     public int duration; //时长
+    public Bitmap albumArt; // 图片
 
-    public Music(String title, String artist, String path, int duration) {
+    public Music(String title, String artist, String path, int duration, Bitmap albumArt) {
         this.title = title;
         this.artist = artist;
         this.path = path;
         this.duration = duration;
+        this.albumArt = albumArt;
     }
 }
--- AndroidStudioProjects/MyMusic/app/src/main/java/com/ted/mymusic/com/ted/mymusic/utils/MediaUtils.java
+++ AndroidStudioProjects/MyMusic/app/src/main/java/com/ted/mymusic/com/ted/mymusic/utils/MediaUtils.java
@@ -1,8 +1,9 @@
 package com.ted.mymusic.com.ted.mymusic.utils;
 
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 
@@ -18,23 +19,41 @@ import java.util.List;
 public class MediaUtils {
     public static List<Music> songList = new ArrayList<>();
 
-    public static void initSongList(Context context){
+    public static void initSongList(Context context) {
         songList.clear();
         L.e("------------------------");
         Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI; // uri
+
+
         String[] projection = {MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ARTIST,
-                MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.DURATION};
+                MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.DURATION, MediaStore.Audio.Media.ALBUM_ID};
         Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
         assert cursor != null;
-        while(cursor.moveToNext()){
+        while (cursor.moveToNext()) {
             String title = cursor.getString(cursor.getColumnIndex(projection[0]));
             String artist = cursor.getString(cursor.getColumnIndex(projection[1]));
             String path = cursor.getString(cursor.getColumnIndex(projection[2]));
             int duration = cursor.getInt(cursor.getColumnIndex(projection[3]));
-
-            Music music = new Music(title, artist, path, duration);
+            String albumID = cursor.getString(cursor.getColumnIndex(projection[4]));
+            Bitmap albumArt = getAlbumArt(context, albumID);
+            Music music = new Music(title, artist, path, duration, albumArt);
             songList.add(music);
         }
         cursor.close();
+    }
+
+    private static Bitmap getAlbumArt(Context context, String albumID) {
+        String mUriAlbums = "content://media/external/audio/albums";
+        Cursor cur = context.getContentResolver().query(Uri.parse(mUriAlbums + "/" + albumID),
+                new String[]{MediaStore.Audio.Albums.ALBUM_ART}, null, null, null);
+        assert cur != null;
+        if (cur.moveToNext()) {
+            String art = cur.getString(0);
+            cur.close();
+            return BitmapFactory.decodeFile(art);
+        } else{
+            cur.close();
+            return null;
+        }
     }
 }
